e3de88d8e0c956960dd98dab687c4114c5d92d78
Napolitain <mxboucher@gmail.com>
2019-03-07 15:44:41 +0100

    gatherData updated! (building levels)

--- modules/execScripts.js
+++ modules/execScripts.js
@@ -6,21 +6,45 @@ let execScripts = {};
 
 execScripts.gatherData = async () => {
 	await common.page.waitFor('#menu-section-general-container > div > div.menu--content-section > div.menu-list-element.habitat-overview--widget.clickable');
+	await common.page.waitFor('.tabular-cell--upgrade-building');
 	await autocorrect.buttonRedAccept();
 	common.castles = await common.page.evaluate(() => {
 		let data = {};
+
+		// get castle resources
 		let castles = document.querySelectorAll('#menu-section-general-container > div > div.menu--content-section > div.menu-list-element.habitat-overview--widget.clickable');
-		for (let i = 0; i < castles.length; i++) {
-			let name = castles[i].querySelector('.habitat-overview-title--habitat-name').innerHTML;
+		for (let castle of castles) {
+			let name = castle.querySelector('.habitat-overview-title--habitat-name').innerHTML;
 			data[name] = {};
-			data[name].points = castles[i].querySelector('.habitat-overview-title--habitat-points').innerHTML;
-			let goods = castles[i].querySelectorAll('.amount'); // the first 6 are sure to be in that order
+			data[name].points = castle.querySelector('.habitat-overview-title--habitat-points').innerHTML;
+			let goods = castle.querySelectorAll('.amount'); // the first 6 are sure to be in that order
 			data[name].wood = goods[0].innerHTML;
 			data[name].stone = goods[1].innerHTML;
-			data[name].copper = goods[2].innerHTML;
-			data[name].farms = goods[3].innerHTML;
-			data[name].bronze = goods[4].innerHTML;
+			data[name].ore = goods[2].innerHTML;
+			data[name].subjects = goods[3].innerHTML;
+			data[name].copper = goods[4].innerHTML;
 			data[name].silver = goods[5].innerHTML;
+		}
+
+		// get buildings levels
+		castles = document.querySelectorAll('.tabular.table--global-overview.table--global-overview--buildings');
+		for (let castle of castles) {
+			let name = castle.querySelector('.tabular-habitat-title-cell--habitat-title').innerHTML;
+			data[name]['buildings'] = {};
+			let buildings = castle.querySelectorAll('.upgrade-building--cell');
+			data[name]['buildings'].keep = buildings[0].querySelector('div').innerHTML;
+			data[name]['buildings'].arsenal = buildings[1].querySelector('div').innerHTML;
+			data[name]['buildings'].tavern = buildings[2].querySelector('div').innerHTML;
+			data[name]['buildings'].library = buildings[3].querySelector('div').innerHTML;
+			data[name]['buildings'].fortification = buildings[4].querySelector('div').innerHTML;
+			data[name]['buildings'].market = buildings[5].querySelector('div').innerHTML;
+			data[name]['buildings'].farm = buildings[6].querySelector('div').innerHTML;
+			data[name]['buildings'].lumberjack = buildings[7].querySelector('div').innerHTML;
+			data[name]['buildings'].woodstore = buildings[8].querySelector('div').innerHTML;
+			data[name]['buildings'].quarry = buildings[9].querySelector('div').innerHTML;
+			data[name]['buildings'].stonestore = buildings[10].querySelector('div').innerHTML;
+			data[name]['buildings'].oremine = buildings[11].querySelector('div').innerHTML;
+			data[name]['buildings'].orestore = buildings[12].querySelector('div').innerHTML;
 		}
 		console.log('4 - execScripts.gatherData() completed');
 		return data;
@@ -31,7 +55,6 @@ execScripts.gatherData = async () => {
 execScripts.build = async () => {
 	for (const [key, value] of Object.entries(common.castles)) {
 		let config = fn.getConfig(value['points']);
-		console.log(config);
 	}
 };
 
