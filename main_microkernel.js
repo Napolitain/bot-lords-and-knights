4b5a8dc21601f54f4db76f02cbb0c96f49ebc6ac
Napolitain <mxboucher@gmail.com>
2019-03-07 18:19:11 +0100

    credentials through parameters instead of config

--- main_microkernel.js
+++ main_microkernel.js
@@ -1,5 +1,8 @@
 /* Main entry of the microkernel based BOT for Lords and Knights
  * https://stackoverflow.com/questions/46346564/how-to-split-code-in-multiple-files-in-node-js
+ *
+ * TODO: autoML.js
+ * TODO: die and retry architecture (if script is done, start the next one, or start one or several times before)
  */
 
 (async () => {
@@ -9,8 +12,11 @@
     const spawnPanels = require('./modules/spawnPanels');
     const execScripts = require('./modules/execScripts');
 
+    const email = process.argv[2];
+    const password = process.argv[3];
+
     await common.init();
-    await fillLogin.precise();
+    await fillLogin.precise(email, password);
 	await submitWorlds.precise();
 	await spawnPanels.precise();
 	await execScripts.gatherData();
--- modules/fillLogin.js
+++ modules/fillLogin.js
@@ -2,14 +2,14 @@ const common = require('./common');
 
 let fillLogin = {};
 
-fillLogin.precise = async () => {
+fillLogin.precise = async (email, password) => {
     await common.page.waitFor("button");
-    await common.page.evaluate(() => {
-        document.querySelector("[name='login-name']").value = "lordk@mail.com";
-        document.querySelector("[name='login-password']").value = "your_password";
+    await common.page.evaluate((email, password) => {
+        document.querySelector("[name='login-name']").value = email;
+        document.querySelector("[name='login-password']").value = password;
         document.querySelector("button").click();
         console.log("1 - fillLogin completed");
-    });
+    }, email, password);
 };
 
 module.exports = fillLogin;
