850089b08cca727caa2739b326de13ebcd545f76
Napolitain <mxboucher@gmail.com>
2019-03-07 10:52:41 +0100

    gatherData updated

--- modules/execScripts.js
+++ modules/execScripts.js
@@ -4,14 +4,14 @@ let execScripts = {};
 
 execScripts.gatherData = async () => {
 	await common.page.waitFor('#menu-section-general-container > div > div.menu--content-section > div.menu-list-element.habitat-overview--widget.clickable');
-	await common.page.evaluate(() => {
+	common.castles = await common.page.evaluate(() => {
 		let data = {};
 		let castles = document.querySelectorAll('#menu-section-general-container > div > div.menu--content-section > div.menu-list-element.habitat-overview--widget.clickable');
 		for (let i = 0; i < castles.length; i++) {
 			let name = castles[i].querySelector('.habitat-overview-title--habitat-name').innerHTML;
 			data[name] = {};
 			data[name].points = castles[i].querySelector('.habitat-overview-title--habitat-points').innerHTML;
-			let goods = castles[i].querySelectorAll('.good');
+			let goods = castles[i].querySelectorAll('.amount'); // the first 6 are sure to be in that order
 			data[name].wood = goods[0].innerHTML;
 			data[name].stone = goods[1].innerHTML;
 			data[name].copper = goods[2].innerHTML;
@@ -20,8 +20,9 @@ execScripts.gatherData = async () => {
 			data[name].silver = goods[5].innerHTML;
 		}
 		console.log('4 - execScripts.gatherData() completed');
-		console.log(JSON.stringify(data));
+		return data;
 	});
+	console.log(common.castles);
 };
 
 execScripts.build = async () => {
