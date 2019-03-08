30b5f8085596c5a0f1a4b002e404b990d2d06249
Napolitain <mxboucher@gmail.com>
2019-03-08 17:04:01 +0100

    added asynchronous behavior to autocorrect module, and hints for the greater good

--- modules/autocorrect.js
+++ modules/autocorrect.js
@@ -3,16 +3,18 @@ const common = require('./common');
 let autocorrect = {};
 
 /**
- * Accept an event with red accept button
+ * Accept an event with red accept button every second asynchronously.
+ * TODO: Make it work as such : every page.evaluate or any moment we need to click it first do that routine. For this, maybe use page.mouse and page.keyboard instead of evaluate, and create a wrapper which calls it.
  * @returns {Promise<void>} ignored
  */
 autocorrect.buttonRedAccept = async () => {
-	await common.page.evaluate(() => {
+	common.page.evaluate(() => {
 		let event = document.querySelector("div.event-pop-up-button.ButtonRedAccept");
 		if (event != null) {
 			event.click();
 			console.log("autocorrect.buttonRedAccept closed an event pop-up");
 		}
+		setTimeout(autocorrect.buttonRedAccept, 1000);
 	});
 };
 
