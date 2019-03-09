29d6c7de305970fee6d40191eac792670f953636
Napolitain <mxboucher@gmail.com>
2019-03-09 17:58:02 +0100

    reorganization

--- modules/scrapping/execScripts.js
+++ modules/scrapping/execScripts.js
@@ -1,6 +1,6 @@
 let common = require('../common');
 common.getTimes = require('./getTimes');
-let fn = require('../fn');
+let fn = require('../data/fn');
 let autocorrect = require('./autocorrect');
 
 common.execScripts = {};
