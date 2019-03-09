799c2183d25259b9f0b04f8fa2caee25b7c7a0be
Napolitain <mxboucher@gmail.com>
2019-03-09 14:28:17 +0100

    beginning FUD typing module

--- modules/io.js
+++ modules/io.js
@@ -1,1 +1,10 @@
 // TODO: make a class which uses page.keyboard.sendCharacter and page.click with human like delays to bypass BOT detectors (may work for reCAPTCHA?)
+
+let common = require('./common');
+common.io = {};
+
+common.io.type = async (text) => {
+	for (const char of text) {
+
+	}
+};
