1b5a776b527edae3b767362b661e1b747adc621b
Napolitain <mxboucher@gmail.com>
2019-03-28 16:01:23 +0100

    common.io.type : focus and typing randomly is done

--- modules/augmented/io.js
+++ modules/augmented/io.js
@@ -3,8 +3,17 @@
 let common = require('../common');
 common.io = {};
 
-common.io.type = async (text) => {
+/**
+ * Focus element and send characters using pseudo random algorithm : random intervals between keypresses with statistics in mind.
+ * @param selector The selector to focus the element.
+ * @param text The text to be sent.
+ * @returns {Promise<void>}
+ */
+common.io.type = async (selector, text) => {
+	common.page.focus(selector);
 	for (const char of text) {
-
+		setTimeout(function () {
+			common.page.keyboard.sendCharacter(char);
+		}, Math.random() * 140 + 110); // probability of IKI (inter-key interval) being between 110 and 250ms is very high
 	}
 };
